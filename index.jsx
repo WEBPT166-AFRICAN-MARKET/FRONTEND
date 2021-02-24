8033d67c54eeba77cb9ae3a5bc3500828e145c4d
Jesse Medrano <Jtamedrano@gmail.com>
2021-02-23 20:59:40 -0800

    Dev (#2)
    * init project with snowpack react
    
    * gitignore updated
    
    * new configuration
    
    * added prettier definitions

--- index.jsx
+++ index.jsx
@@ -1,10 +1,14 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import './scss/indexStyle.scss';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
