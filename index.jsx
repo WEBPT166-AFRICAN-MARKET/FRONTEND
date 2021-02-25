1662abf9a884e4f18c7f5c0b7703ef3e4698a12b
Jesse Medrano <Jtamedrano@gmail.com>
2021-02-24 18:28:33 -0800

    redux added

--- index.jsx
+++ index.jsx
@@ -2,13 +2,16 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './scss/indexStyle.scss';
+import { Provider } from 'react-redux';
+import { store } from './redux/store';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Provider store={store}>
+			<Router>
+				<App />
+			</Router>
+		</Provider>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
