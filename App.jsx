af8520ee0e1c07fae5fbee19ebeffded5df88e92
Jesse Medrano <Jtamedrano@gmail.com>
2021-02-24 18:30:38 -0800

    redux added (#3)

--- App.jsx
+++ App.jsx
@@ -5,11 +5,11 @@ import { Route } from 'react-router';
 import Root from './views/Root';
 
 const App = () => {
-  return (
-    <div>
-      <Route exact path="/" component={Root} />
-    </div>
-  );
+	return (
+		<div>
+			<Route exact path="/" component={Root} />
+		</div>
+	);
 };
 
 export default App;
