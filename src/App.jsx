b5972e5cfd763eadf40ca2f2fcab4fce0eb59b07
unknown <allenmk20073@yahoo.com>
2021-02-25 23:11:48 -0500

    useStates and Inputs setup. Moving on to onChange.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import { getSelectors } from './selectors';
 // View Components
 import Root from './views/Root';
 import UserHome from './views/UserHome';
+import LoginView from './views/Login';
 
 const App = () => {
 	const { isAuthenticated } = getSelectors().user;
@@ -13,7 +14,8 @@ const App = () => {
 		<div className="app">
 			<Container maxWidth="lg">
 				<Route exact path="/">
-					{isAuthenticated ? <UserHome /> : <Root />}
+					{isAuthenticated ? <UserHome /> : <Root /> }
+					<LoginView />
 				</Route>
 			</Container>
 		</div>
--- src/views/Login/index.jsx
+++ src/views/Login/index.jsx
@@ -1,10 +1,118 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import * as yup from 'yup';
+import { useHistory } from 'react-router-dom';
 
-const LoginView = () => {
+const LoginView = (props) => {
+
+  const [formState, setFormState] = useState({
+    name: '',
+    email: '',
+    username: '',
+    password: '',
+    passwordConfirm: '',
+    location: '',
+    terms: true,
+  })
+
+  const [buttonDisabled, setButtonDisabled] = useState(true)
+
+  const [errors, setErrors] = useState({
+    name: '',
+    email: '',
+    username: '',
+    password: '',
+    passwordConfirm: '',
+    location: '',
+    terms: '',
+  })
+
+
+
   return (
-    <div>
-      <h1>Login View</h1>
-    </div>
+    <form>
+      <h1>New Seller Registration!</h1>
+      <p>Welcome! Please fill out the following information to continue.</p>
+
+
+      <input 
+        id='name'
+        type='text'
+        name='name'
+        value={formState.name}
+        placeholder='Name'
+        onChange='name'
+        data-cy='name'
+      />
+
+        {errors.name.length > 0 ? <p className='error'>{errors.name}</p> : null}
+
+      <input 
+        id='username'
+        type='text'
+        name='username'
+        value={formState.userName}
+        placeholder='Username'
+        onChange="name"
+        data-cy='username'
+      />
+
+      <input 
+        id='email'
+        type='text'
+        name='email'
+        value={formState.email}
+        placeholder='Email'
+        onChange='name'
+        data-cy='email'
+      />
+
+      <input 
+        id='password'
+        type='password'
+        name='password'
+        value={formState.password}
+        placeholder='Password'
+        onChange='name'
+        data-cy='password'
+      />
+
+      <input 
+        id='passwordConfirm'
+        type='password'
+        name='passwordConfirm'
+        value={formState.passwordConfirm}
+        placeholder='Please Confirm Password'
+        onChange='name'
+        data-cy='passwordConfirm'
+      
+      />
+
+      <input 
+        id='location'
+        type='text'
+        name='location'
+        value={formState.location}
+        placeholder='Location'
+        onChange='name'
+        data-cy='location'
+      />
+
+      <label htmlFor='terms' className='terms'>
+        <input
+          id='terms' 
+          type='checkbox'
+          name='terms'
+          checked={formState.terms}
+          onChange="Change this later"
+          data-cy='terms'
+        />
+        Terms and Conditions
+      </label>
+
+      <button disabled={buttonDisabled} type='submit'>Submit</button>
+      
+
+    </form>
   );
 };
 
