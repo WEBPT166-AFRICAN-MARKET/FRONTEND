d9cc33a44efece6c804b71628b771b1986feae0b
Jesse Medrano <Jtamedrano@gmail.com>
2021-03-05 19:49:48 -0800

    Backdrop and add Item Form (Not Connected) (#18)

--- src/views/UserHome/index.jsx
+++ src/views/UserHome/index.jsx
@@ -7,7 +7,10 @@ import {
 	Tab,
 	makeStyles,
 	Fab,
-	Typography
+	Typography,
+	Backdrop,
+	TextField,
+	Button
 } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
 
@@ -16,10 +19,20 @@ import Panel from './Panel';
 import { fetchItems } from '../../redux/actions/actions';
 
 const useStyles = makeStyles(theme => ({
+	backdrop: {
+		zIndex: theme.zIndex.drawer + 1
+	},
 	fab: {
 		position: 'absolute',
 		bottom: theme.spacing(8),
 		right: theme.spacing(8)
+	},
+	form: {
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'start',
+		justifyContent: 'center',
+		zIndex: theme.zIndex.drawer + 2
 	}
 }));
 
@@ -29,7 +42,13 @@ const UserHome = () => {
 	const dispatch = useDispatch();
 	const store = getSelectors().items.items;
 
+	const [addItemModalState, setAddItemModalState] = useState(false);
 	const [tabState, setTabState] = useState(0);
+
+	const toggleModal = e => {
+		e.preventDefault();
+		setAddItemModalState(!addItemModalState);
+	};
 
 	const handleChange = (event, newValue) => {
 		event.preventDefault();
@@ -43,50 +62,87 @@ const UserHome = () => {
 	console.log('Store => ', store);
 
 	return (
-		<motion.div className="userHomePage">
-			<Grid id="userHomePanel" container direction="row">
-				<Box mx={'auto'} borderBottom={1} p={1} width="100%">
-					<Grid item>
-						<Grid
-							container
-							alignItems="center"
-							justify="space-between"
-							spacing={1}
-						>
-							<h1>Hello {username}</h1>
-							<Tabs value={tabState} onChange={handleChange}>
-								<Tab label="store" {...a11yProps(0)} />
-								<Tab label="My Store" {...a11yProps(1)} />
-								<Tab label="My Profile" {...a11yProps(2)} />
-							</Tabs>
+		<>
+			<motion.div className="userHomePage">
+				<Grid id="userHomePanel" container direction="row">
+					<Box mx={'auto'} borderBottom={1} p={1} width="100%">
+						<Grid item>
+							<Grid
+								container
+								alignItems="center"
+								justify="space-between"
+								spacing={1}
+							>
+								<h1>Hello {username}</h1>
+								<Tabs value={tabState} onChange={handleChange}>
+									<Tab label="store" {...a11yProps(0)} />
+									<Tab label="My Store" {...a11yProps(1)} />
+									<Tab label="My Profile" {...a11yProps(2)} />
+								</Tabs>
+							</Grid>
 						</Grid>
-					</Grid>
-				</Box>
-				<Box
-					className="userHomeGridItem"
-					bgcolor="background.lighter"
-					p={1}
-				>
-					<TabPanel value={tabState} index={0}>
-						<Panel store={store} />
-					</TabPanel>
-					<TabPanel value={tabState} index={1}>
-						<h2>My Store</h2>
-					</TabPanel>
-					<TabPanel value={tabState} index={2}>
-						<h2>Username: {username}</h2>
-					</TabPanel>
-					<Fab
-						aria-label="Add Item"
-						className={classes.fab}
-						color="primary"
-						variant="extended"
+					</Box>
+					<Box
+						className="userHomeGridItem"
+						bgcolor="background.lighter"
+						p={1}
 					>
-						Add Item
-					</Fab>
+						<TabPanel value={tabState} index={0}>
+							<Panel store={store} />
+						</TabPanel>
+						<TabPanel value={tabState} index={1}>
+							<h2>My Store</h2>
+						</TabPanel>
+						<TabPanel value={tabState} index={2}>
+							<h2>Username: {username}</h2>
+						</TabPanel>
+						<Fab
+							aria-label="Add Item"
+							className={classes.fab}
+							color="primary"
+							variant="extended"
+							onClick={toggleModal}
+						>
+							Add Item
+						</Fab>
+					</Box>
+				</Grid>
+			</motion.div>
+			<Backdrop
+				className={classes.backdrop}
+				open={addItemModalState}
+				onClick={toggleModal}
+			>
+				<Box
+					bgcolor="#555"
+					boxShadow="0 0 5px #fff"
+					display="flex"
+					justifyContent="center"
+					alignItems="center"
+					flexDirection="column"
+					width="50%"
+					height="50%"
+					borderRadius="1em"
+					onClick={e => {
+						e.stopPropagation();
+					}}
+				>
+					<h2>Add An Item</h2>
+					<form
+						className={classes.form}
+						onSubmit={e => {
+							e.preventDefault();
+						}}
+					>
+						<TextField label="Item Name" variant="filled" />
+						<TextField label="Price" variant="filled" />
+						<Button type="submit" style={{ margin: '1em auto' }}>
+							Submit
+						</Button>
+					</form>
 				</Box>
-			</Grid>
-		</motion.div>
+			</Backdrop>
+		</>
 	);
 };
 
