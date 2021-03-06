19eb4be97c3e856d1b7428d155367254f9291949
Jesse Medrano <Jtamedrano@gmail.com>
2021-03-06 10:28:50 -0800

    Update index.jsx

--- src/views/Login/index.jsx
+++ src/views/Login/index.jsx
@@ -3,7 +3,6 @@ import * as yup from 'yup';
 import { useHistory } from 'react-router-dom';
 import { login } from '../../redux/actions/actions';
 
-import './Login.css';
 import { useDispatch } from 'react-redux';
 
 const LoginView = props => {
